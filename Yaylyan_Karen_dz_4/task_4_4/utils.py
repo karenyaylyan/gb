3bbc61b46358d698dfb0178b4f43fa2f0b09c45b
Yaylyan Karen <karen.yaylyan@mail.ru>
2022-06-02 21:42:11 +0300

    dz4

--- Yaylyan_Karen_dz_4/task_4_4/utils.py
+++ Yaylyan_Karen_dz_4/task_4_4/utils.py
@@ -20,3 +20,4 @@ def currency_rates(valute_code):
 
     return None
     
+    
--- Yaylyan_Karen_dz_4/task_4_5/utils.py
+++ Yaylyan_Karen_dz_4/task_4_5/utils.py
@@ -19,4 +19,5 @@ def currency_rates(valute_code):
         return valute_result, date_result
 
     return None
+
     
