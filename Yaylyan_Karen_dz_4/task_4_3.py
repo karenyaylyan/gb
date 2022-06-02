d056ce10d4258ee24be38974d8911685deb7f76e
Yaylyan Karen <karen.yaylyan@mail.ru>
2022-06-02 21:23:55 +0300

    dz4

---
+++ Yaylyan_Karen_dz_4/task_4_3.py
@@ -0,0 +1,25 @@
+import requests
+import json
+from datetime import datetime
+from decimal import Decimal
+
+
+def currency_rates(valute_code):
+    url = 'https://www.cbr-xml-daily.ru/daily_json.js'
+    response = requests.get(url)
+    valutes = json.loads(response.content)
+
+    valute_code = valute_code.upper()
+    if valute_code in valutes['Valute']:
+        valute_result = Decimal(valutes['Valute'][valute_code]['Value']).quantize(Decimal('1.0000'))
+
+        date_str = response.headers['Date']
+        date_result = datetime.strptime(date_str, '%a, %d %b %Y %H:%M:%S GMT')
+
+        return valute_result, date_result
+
+    return None
+
+if __name__ == '__main__':
+    print(currency_rates('usd'))
+    print(currency_rates('EUR'))
---
+++ Yaylyan_Karen_dz_4/task_4_4/task_4_4.py
@@ -0,0 +1,6 @@
+import utils
+
+print(utils.currency_rates('usd'))
+print(utils.currency_rates('EUR'))
+print(utils.currency_rates('Jpy'))
+print(utils.currency_rates('not_valute'))
